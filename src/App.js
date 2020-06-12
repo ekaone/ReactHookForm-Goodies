b4856038b495592c1aef7d2deb2963210cb54b79
Eka Prasetia <ekaone3033@gmail.com>
2020-06-12 22:24:51 +0700

    init commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "./mvp.css";
 
 function App() {
   return (
