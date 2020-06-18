7dc1fb607405cd4de921de117e3cdeb4ddd26a31
Eka Prasetia <ekaone3033@gmail.com>
2020-06-18 14:39:16 +0700

    Updates

---
+++ precache-manifest.aa371ff3332fd7377e389c44f52b55f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eb394ded26e92281ab8b248aebb04e4",
+    "url": "/ReactHookForm-Goodies/index.html"
+  },
+  {
+    "revision": "ed2f572d8cd422ae7011",
+    "url": "/ReactHookForm-Goodies/static/css/main.156da71e.chunk.css"
+  },
+  {
+    "revision": "3df3697b4e8b0d1520d7",
+    "url": "/ReactHookForm-Goodies/static/js/2.020faddb.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/ReactHookForm-Goodies/static/js/2.020faddb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed2f572d8cd422ae7011",
+    "url": "/ReactHookForm-Goodies/static/js/main.8032dfe7.chunk.js"
+  },
+  {
+    "revision": "4550fbb018970a961f2f",
+    "url": "/ReactHookForm-Goodies/static/js/runtime-main.5bf696d4.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/ReactHookForm-Goodies/precache-manifest.aa371ff3332fd7377e389c44f52b55f6.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ReactHookForm-Goodies/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
